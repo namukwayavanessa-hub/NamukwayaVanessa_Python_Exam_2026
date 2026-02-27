5758ebd65f4729be6ef11a41500174a873a1a4cb
namukwayavanessa <kimoliza321@gmail.com>
2026-02-27 11:44:09 +0300

    END OF SEM 1 work

--- question4.py
+++ question4.py
@@ -1,28 +1,37 @@
 #question4(i)
 # OOP is a programming style that organizes code using objects and classes
 
-#  importance 
+#importance 
 # OOP is fast in we development
-#easy maintance
-#resuability
+#It's easy maintance
+#ResuabilityInheritance is a core OOP concept where one class (child/subclass) acquires the properties and behaviors (fields and methods) of another class (parent/superclass)
+# Real life idea
 
+# A Vehicle can move and has wheels.
+
+# A Car is a type of Vehicle, but it also has extra features like doors and air conditioning
 
 #question4(ii)
-text=str(input('enter a sentence:'))
-vowels = "aeiouAEIOU"
-count = 0
-for char in text:
-    if char in vowels:
-        count += 1
-print(" The number of vowels in the entered text is ", count)
+# text=str(input('enter a sentence:'))
+# vowels = "aeiouAEIOU"
+# count = 0
+# for char in text:
+#     if char in vowels:
+#         count += 1
+# print(" The number of vowels in the entered text is ", count)
 
 
-#quetion4(iii)
-def numbers(x , y):
-    product= x * y
-    return product
-print( "The function with values x = 6 and y= 7 its product is " ,numbers(6 ,7))
+# #quetion4(iii)
+# def numbers(x , y):
+#     product= x * y
+#     return product
+# print( "The function with values x = 6 and y= 7 its product is " ,numbers(6 ,7))
 
 # #question4(iv)
-# input('class')
+class Latptop:
+             def __init__(self, brand, model, price): self.brand = brand , self.model = model ,self.price = price
+             Latptop = Latptop("del", "m890",7800)
+print(Latptop.brand, Latptop.model, Latptop.price)
+
+ 
     
